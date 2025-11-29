5eefda4389a8ee995eb90771a69c2eaed04b7471
shazakhaled <shazakhaled807@gmail.com>
2025-11-29 19:46:54 +0200

    Updated_Backend

---
+++ src/test/java/com/ClubsManagementSystem/NewHomePage/NewHomePageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ClubsManagementSystem.NewHomePage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewHomePageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
